b71ecf1b52a554e6605cfc3c156711dc7362a2ea
DonCarlo31993 <karolkojro@gmail.com>
2019-12-29 23:33:19 +0100

    Zabawa z FX'em

---
+++ src/main/java/main/MainWindow.java
@@ -0,0 +1,25 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainWindow extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Projekt okno");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
